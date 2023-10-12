78d87dee1519cced19e2e03d4a56e59b30407fca
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-10-12 19:37:26 +0100

    projects updated

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-const navToggle = document.querySelector(".nav_toggle");
-const navLinks = document.querySelectorAll(".nav_link");
+const navToggle = document.querySelector(".nav_toggle")
+const navLinks = document.querySelectorAll(".nav_link")
+const jsProjectsEl = document.getElementById("js-projects")
+console.log(jsProjectsEl)
 
 navToggle.addEventListener("click", () => {
-    document.body.classList.toggle("nav_open");
+    document.body.classList.toggle("nav_open")
 });
 
 navLinks.forEach((link) => {
     link.addEventListener("click", () => {
-        document.body.classList.remove("nav_open");
-    });
-});
+        document.body.classList.remove("nav_open")
+    })
+})
+
+
